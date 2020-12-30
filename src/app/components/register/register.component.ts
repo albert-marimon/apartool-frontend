79e50f5e0b334964e512daee161998d314f64c91
albert-marimon <info@albertmarimon.com>
2020-12-30 13:27:41 +0100

    Registro de usuario

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css'],
+	providers: [UserService]
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+export class RegisterComponent {
+	public page_title: string;
+	public user: User;
+  	public success: boolean;
 
-  ngOnInit(): void {
-  }
 
+  	constructor(private _userService: UserService) {
+  		this.page_title = "Regístrate";
+  		this.user = new User('', '', '', '', '', '', 0);
+    }
+    
+  	onSubmit(form){
+  		this._userService.register(this.user).subscribe(
+  			response => {
+				if(response.success){
+					this.success = response.success;
+					form.reset();
+				} else {
+					this.success = false;
+				}
+  			},
+  			error => {
+          		this.success = false;
+  				console.log(<any>error);
+  			}
+  		);
+  	}
 }
+
