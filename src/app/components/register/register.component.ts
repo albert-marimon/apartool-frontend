ec61bac3082c43cf2434cad96b54e670619ea898
albert-marimon <info@albertmarimon.com>
2020-12-30 15:27:05 +0100

    toaster en registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
 	selector: 'register',
@@ -15,26 +18,39 @@ export class RegisterComponent {
   	public success: boolean;
 
 
-  	constructor(private _userService: UserService) {
+  	constructor(
+		private _userService: UserService,
+		private router: Router,
+		private _snackBar: MatSnackBar,
+	) {
   		this.page_title = "Regístrate";
-  		this.user = new User('', '', '', '', '', '', 0);
+  		this.user = new User('', '', '', '', '', '', 1);
     }
     
   	onSubmit(form){
   		this._userService.register(this.user).subscribe(
   			response => {
-				if(response.success){
+				if(response.status == 'success'){
 					this.success = response.success;
 					form.reset();
+					this.showToastInfo('Usuario registrado correctamente', 'X');
+					this.router.navigate(['/login']);
 				} else {
 					this.success = false;
+					this.showToastInfo('Usuario registrado correctamente', 'X');
 				}
   			},
   			error => {
           		this.success = false;
-  				console.log(<any>error);
+				this.showToastInfo(<any>error, 'X');
   			}
   		);
-  	}
+	  }
+	  
+	showToastInfo(message, action){
+		this._snackBar.open(message, action, {
+			duration: 5000,
+		});
+	}
 }
 
