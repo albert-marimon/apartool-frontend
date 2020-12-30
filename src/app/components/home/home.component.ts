448113c5b89e7524b5c3528467110cee5f533214
albert-marimon <info@albertmarimon.com>
2020-12-30 13:25:54 +0100

    Optimización código home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { global } from '../../services/global';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-//import { ToastrService } from 'ngx-toastr';
-//import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public token: string;
+  public identity: {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _userService: UserService, private router: Router) {
+    this.loadUser();
+    if (this.token) {
+		  router.navigate(['/users/list']);
+		} else {
+      router.navigate(['/login']);
+    }
   }
+
+  loadUser(){
+		this.identity = this._userService.getIdentity();
+		this.token = this._userService.getToken();
+	}
 
 }
